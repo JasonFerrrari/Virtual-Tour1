a3e8560375b8b5da4d89f5f894d2633026576007
JasonFerrrari <jason.ferrari@uconn.edu>
2021-01-16 13:40:04 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,218 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -2.948484320508788,
+        "pitch": 0.17229906486035063,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.687017306328281,
+        "pitch": 0.1762503915139817,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5882416232881695,
+          "pitch": 0.10520450763023703,
+          "rotation": 0,
+          "target": "2-5"
+        },
+        {
+          "yaw": 2.114661846099982,
+          "pitch": 0.16875188972888822,
+          "rotation": 0,
+          "target": "3-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.3573779023711552,
+        "pitch": 0.14692312508248584,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.007597451803730948,
+          "pitch": 0.10968133850188444,
+          "rotation": 0,
+          "target": "1-3"
+        },
+        {
+          "yaw": 1.1257675651349022,
+          "pitch": -0.03819780985246268,
+          "rotation": 0,
+          "target": "3-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.04027682889217843,
+        "pitch": 0.007351917794721885,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.32018467875985657,
+          "pitch": -0.0698213190569188,
+          "rotation": 0,
+          "target": "2-5"
+        },
+        {
+          "yaw": 0.3466327125704005,
+          "pitch": 0.0676465687971799,
+          "rotation": 0,
+          "target": "1-3"
+        },
+        {
+          "yaw": -2.6185216216674156,
+          "pitch": 0.09541925229151715,
+          "rotation": 0,
+          "target": "4-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-7",
+      "name": "7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.5799884775863671,
+        "pitch": 0.13601047920237264,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12435433012077546,
+          "pitch": 0.1191657546733147,
+          "rotation": 0,
+          "target": "3-6"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "TGM - Living Room + Kitchen",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
